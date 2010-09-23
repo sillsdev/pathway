3e23960f31656bf3867941391d3337c8b0823099
Greg Trihus <gtrihus59@yahoo.com>
2010-09-23 14:27:52 +0000

    0.6.6 Update

--- pathway/LiftPrepare/LiftPreparer.cs
+++ pathway/LiftPrepare/LiftPreparer.cs
@@ -1,15 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Text;
 using System.Windows.Forms;
 using System.Xml;
-using System.Xml.Xsl;
-using SIL.PublishingSolution;
 using SIL.PublishingSolution.Filter;
 using SIL.PublishingSolution.Sort;
 
