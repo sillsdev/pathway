56a89a5420fafa6a19f4b1a9a344dd524c2fa16a
Greg Trihus <gtrihus59@yahoo.com>
2010-10-21 04:15:09 +0000

    Sprint 0.6.9 installer

--- pathway/CssParser/CssParserPostBuild.bat
+++ pathway/CssParser/CssParserPostBuild.bat
@@ -1,2 +1,6 @@
-set classpath=..\..\jars\antlr-3.1.3.jar;..\..\jars\antlr-runtime-3.2.jar;..\..\jars\stringtemplate-3.2.1.jar;..\..\jars\antlr-2.7.7.jar
-java org.antlr.Tool ..\..\csst3.g
+set base=..\..
+if exist %base%\csst3.g goto anyCpu
+set base=..\..\..
+:anyCpu
+set classpath=%base%\jars\antlr-3.1.3.jar;%base%\jars\antlr-runtime-3.2.jar;%base%\jars\stringtemplate-3.2.1.jar;%base%\jars\antlr-2.7.7.jar
+java org.antlr.Tool %base%\csst3.g
