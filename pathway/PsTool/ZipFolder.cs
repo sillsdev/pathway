8a777beab81189dd79572a378700a7605d093560
Erik Brommers <ebrommers@gmail.com>
2010-11-02 15:07:08 +0000

    New method, AddToZip(), that adds a file to an existing archive.

--- pathway/PsTool/ZipFolder.cs
+++ pathway/PsTool/ZipFolder.cs
@@ -30,6 +30,45 @@ namespace SIL.Tool
     /// </summary>
     public class ZipFolder
     {
+        #region AddToZip(string filename, string archive, Boolean addCompresed)
+        /// <summary>
+        /// Add a single file to an existing Zip archive, with the option of adding the file
+        /// as deflated or stored (no compression). As of 10/19/2010, these are the only compression 
+        /// options supported by the ICSharpCode library.
+        /// Note: Files are currently added to the root of the archive.
+        /// </summary>
+        /// <param name="filename">Path and filename of the file to add to the archive</param>
+        /// <param name="archive">Zip archive that we want to add the file to</param>
+        /// <param name="addCompresed">true if compressed, false if stored</param>
+        public void AddToZip(string filename, string archive, Boolean addCompresed)
+        {
+            if (!File.Exists(filename) || !File.Exists(archive))
+            {
+                return;
+            }
+
+            try
+            {
+
+                ZipFile zf = new ZipFile(archive);
+                zf.BeginUpdate();
+                // NameTransform nukes the path info from Filename (so it gets added to the top level of the archive)
+                zf.NameTransform = new ZipNameTransform(Path.GetDirectoryName(filename));
+                zf.Add(filename, (addCompresed) ? CompressionMethod.Deflated : CompressionMethod.Stored);
+                zf.CommitUpdate();
+                zf.Close();
+            }
+            catch (Exception e)
+            {
+                if (e.Message != null)
+                {
+                    var msg = new[] { e.Message };
+                    LocDB.Message("defErrMsg", e.Message, msg, LocDB.MessageTypes.Error, LocDB.MessageDefault.First);
+                }
+            }
+        }
+        #endregion
+
         #region CreateZip(string sourceFolder, string outputFileWithPath, int errCount)
         /// <summary>
         /// Compress Generated file
