0310aec9fc3977985a5f0619d85279f4d172373b
Philip Perry <p.r.perry>
2011-01-06 07:36:58 +0000

    moved search form code into searchform.php

--- pathway/WordPressThemes/Dictionary/Trilingual/search.php
+++ pathway/WordPressThemes/Dictionary/Trilingual/search.php
@@ -4,36 +4,11 @@
     <div id="yui-main">
         <div class="yui-b" >
 			<div class="yui-ge"><div class="yui-u first">
-
-		   		<table>	
-		   		<tr> 
-		   			<td><h3>Look up English, Chinese or Nuosu Yi</h3>
-						<br>
-						&nbsp;
-				   		<a href=""><?php _e('[:en]Yi Index Chart[:zh]&#24413;&#25991;&#38899;&#24207;&#26816;&#23383;&#34920;');?></a>				   		
-						&nbsp;&nbsp;
-				   		<a href=""><?php _e('[:en]Radical Stroke Index[:zh]&#24413;&#25991;&#37096;&#39318;&#26816;&#23383;&#34920;');?></a>
-		   			</td>
-
-				</tr>
-		         <tr>
-		   			<td>
-				 	     <form id="searchform" method="get" action="<?php bloginfo('url'); ?>"> 
-							<input type="text" name="s" id="s" value="<?php the_search_query(); ?>" size=40>
-							<input type="hidden" name="lang" value="en">
-							<input type="submit" id="searchsubmit" name="search" value="Search" />
-							<select name="key">
-								<option value=""></option>
-								<option value="NuosuCharacter">Nuosu Character</option>
-								<option value="NuosuTransliteration">Nuosu Transliteration</option>
-							</select>							
-			 	     	 </form>
-		   			</td>
-		   		</tr>
-		   		</table>  	
-                        
+			                        
  
         <?php 
+        require("searchform.php");
+        
         if (have_posts()) : ?>
 						<div class="yui-ge">Search results for '<?php the_search_query(); ?>':</div>
                 <?php while (have_posts()) : the_post(); ?>
