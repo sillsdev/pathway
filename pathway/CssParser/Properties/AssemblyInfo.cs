126afbeecd895f18e0410f2a13d8daab0bd2344c
Greg Trihus <gtrihus59@yahoo.com>
2011-01-18 15:00:51 +0000

    Update release builds to include epubValidator

--- pathway/CssParser/Properties/AssemblyInfo.cs
+++ pathway/CssParser/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.14.435")]
-[assembly: AssemblyFileVersion("0.6.14.435")]
+[assembly: AssemblyVersion("0.6.14.447")]
+[assembly: AssemblyFileVersion("0.6.14.447")]
--- pathway/PublishingSolutionExe/Properties/AssemblyInfo.cs
+++ pathway/PublishingSolutionExe/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.14.435")]
-[assembly: AssemblyFileVersion("0.6.14.435")]
+[assembly: AssemblyVersion("0.6.14.447")]
+[assembly: AssemblyFileVersion("0.6.14.447")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
