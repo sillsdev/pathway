05d61e816cd5f42ce60707fded6ace520df999be
Greg Trihus <gtrihus59@yahoo.com>
2011-01-25 14:30:56 +0000

    latest daily build

--- pathway/ParatextSupport/Properties/AssemblyInfo.cs
+++ pathway/ParatextSupport/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.6.15.465*")]
-[assembly: AssemblyVersion("0.6.15.465")]
-[assembly: AssemblyFileVersion("0.6.15.465")]
+// [assembly: AssemblyVersion("0.6.15.493*")]
+[assembly: AssemblyVersion("0.6.15.493")]
+[assembly: AssemblyFileVersion("0.6.15.493")]
--- pathway/Test/Properties/AssemblyInfo.cs
+++ pathway/Test/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.6.15.465")]
-[assembly: AssemblyFileVersion("0.6.15.465")]
+[assembly: AssemblyVersion("0.6.15.493")]
+[assembly: AssemblyFileVersion("0.6.15.493")]
