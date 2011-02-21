0db5b7277553d0c27b2e166568cf9decc9572c19
Greg Trihus <gtrihus59@yahoo.com>
2011-02-21 17:51:38 +0000

    TD-1957 Use correct help file for different editions

--- pathway/CssDialog/PrintVia604.cs
+++ pathway/CssDialog/PrintVia604.cs
@@ -237,8 +237,7 @@ namespace SIL.PublishingSolution
             LoadExtraProcessing();
             LoadProperty();
             txtSaveInFolder.Text = Common.GetSaveInFolder(Param.DefaultValue[Param.PublicationLocation], DatabaseName, cmbSelectLayout.Text);
-            var iType = true;
-            Common.PathwayHelpSetup(iType, Common.FromRegistry("Help"));
+            Common.PathwayHelpSetup();
             Common.HelpProv.SetHelpNavigator(this, HelpNavigator.Topic);
             Common.HelpProv.SetHelpKeyword(this, _helpTopic);
             Common.databaseName = DatabaseName;
@@ -550,11 +549,7 @@ namespace SIL.PublishingSolution
 
         private void BtnHelp_Click(object sender, EventArgs e)
         {
-            var helpFolder = Common.FromRegistry("Help");
-            var directoryInfo = new DirectoryInfo(helpFolder);
-            var fileInfoList = directoryInfo.GetFiles("*BTE.chm");
-            var iType = fileInfoList.Length > 0;
-            Common.PathwayHelpSetup(iType, helpFolder);
+            Common.PathwayHelpSetup();
             Common.HelpProv.SetHelpNavigator(this, HelpNavigator.Topic);
             Common.HelpProv.SetHelpKeyword(this, _helpTopic);
             SendKeys.Send("{F1}");
