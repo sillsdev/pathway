d1c5c5657e732fa2f7d89ec6f1e6ca4e0e9c04a2
Greg Trihus <gtrihus59@yahoo.com>
2011-02-21 17:04:36 +0000

    TD-1957 Use correct help file for different editions

--- pathway/CssDialog/PrintVia.cs
+++ pathway/CssDialog/PrintVia.cs
@@ -549,8 +549,11 @@ namespace SIL.PublishingSolution
 
         private void BtnHelp_Click(object sender, EventArgs e)
         {
-            var iType = InputType.ToLower() != "scripture";
-            Common.PathwayHelpSetup(iType, Common.FromRegistry("Help"));
+            var helpFolder = Common.FromRegistry("Help");
+            var directoryInfo = new DirectoryInfo(helpFolder);
+            var fileInfoList = directoryInfo.GetFiles("*BTE.chm");
+            var iType = fileInfoList.Length > 0;
+            Common.PathwayHelpSetup(iType, helpFolder);
             Common.HelpProv.SetHelpNavigator(this, HelpNavigator.Topic);
             Common.HelpProv.SetHelpKeyword(this, _helpTopic);
             SendKeys.Send("{F1}");
