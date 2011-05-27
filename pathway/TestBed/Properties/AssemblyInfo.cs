ac41922a47ef8086e5edbff49f425c6c49cb2f1e
Greg Trihus <gtrihus59@yahoo.com>
2011-05-27 05:37:32 +0000

    Update Version numbers and readme for new build

--- pathway/TestBed/Properties/AssemblyInfo.cs
+++ pathway/TestBed/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.2.1065")]
-[assembly: AssemblyFileVersion("0.7.2.1065")]
+[assembly: AssemblyVersion("0.7.3.1168")]
+[assembly: AssemblyFileVersion("0.7.3.1168")]
