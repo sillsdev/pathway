d2183f2dfb99fb8d1157cc6ffbe98439b087a51d
Greg Trihus <gtrihus59@yahoo.com>
2011-07-05 20:45:00 +0000

    0.7.6 sprint build changes

--- pathway/mynant.bat
+++ pathway/mynant.bat
@@ -2,4 +2,4 @@
 rem works with Nant 0.91 with the additional contributed task of msbuild
 rem see http://nant.sourceforge.net/release/0.85/help/index.html
 rem see http://nantcontrib.sourceforge.net/release/0.85-rc4/help/index.html
-c:\perforce\ww\bin\nant\bin\nant %1 %2 %3 %4 %5 %6
+c:\perforce\fw\ww\bin\nant\bin\nant %1 %2 %3 %4 %5 %6
