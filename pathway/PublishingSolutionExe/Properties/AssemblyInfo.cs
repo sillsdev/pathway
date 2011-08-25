35d12f00647b593d37256d8057e59438564725ef
Greg Trihus <gtrihus59@yahoo.com>
2011-08-25 19:01:45 +0000

    Clean before each installer build for sprint

--- pathway/PublishingSolutionExe/Properties/AssemblyInfo.cs
+++ pathway/PublishingSolutionExe/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.7.9.1500")]
-[assembly: AssemblyFileVersion("0.7.9.1500")]
+[assembly: AssemblyVersion("0.7.9.1507")]
+[assembly: AssemblyFileVersion("0.7.9.1507")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
