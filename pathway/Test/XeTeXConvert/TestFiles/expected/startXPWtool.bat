eda433096d6b2cf366bf644a338c143f92ddb0ec
Greg Trihus <gtrihus59@yahoo.com>
2011-09-19 09:26:14 +0000

    Remove XeTexConvert (for export using ConTeXt

--- pathway/Test/XeTeXConvert/TestFiles/expected/startXPWtool.bat
+++
@@ -1,7 +0,0 @@
-rem startXPWtool-tpl.bat - template for startXPWtool.bat
-C:
-cd C:\PwCtx
-call "%cd%\miniCTX\setuptex.bat"
-C:
-cd C:\svn\btai\PublishingSolution\Test\XeTeXConvert\TestFiles\output
-"%CD%\XPWtool.bat"
