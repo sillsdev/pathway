090104842fe4ce163995d4062e4d8a52d278a9ce
Greg Trihus <gtrihus59@yahoo.com>
2012-03-03 10:38:35 +0000

    Update Test project to move PostTestBuild.bat into Test.csproj

--- pathway/Test/PostTestBuild.bat
+++
@@ -1,7 +0,0 @@
-set src=..\..\..
-if exist %src%\Test\PostTestBuild.bat goto anyCPU
-set src=..\..\..\..
-:anyCpu
-copy %src%\LiftPrepare\Lib\PalasoLib\icu??40.dll . /d /y
-rem xcopy ..\..\PublishingSolutionExeUi\*.xml . /d /y
-xcopy %src%\ThirdParty\epubcheck-1.2\* .\epubcheck-1.2  /i /s /q /y
