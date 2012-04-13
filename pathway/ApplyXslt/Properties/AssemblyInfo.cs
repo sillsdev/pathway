5bfd87e19ea4ef1e2976104cc054c79557307818
gtryus <gtryus@gmail.com>
2012-04-13 11:17:20 -0500

    Update copyright message

--- pathway/ApplyXslt/Properties/AssemblyInfo.cs
+++ pathway/ApplyXslt/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("ApplyXslt")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © SIL International 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
