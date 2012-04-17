bca366e7d65095fe7007b18eeef4de8d475b2295
gtryus <gtryus@gmail.com>
2012-04-17 10:05:48 -0500

    Setup to adjust grammar
    Grammar can be adjusted and tested interactively
    with AntlrWorks. Then CssParserPostBuild will
    generate the C# files. Note that to test with Java,
    it is necessary to comment out the language=CSharp
    in the header and the @members declaration. (Also
    the path may need to be adjusted in AntlrWorks for
    your machine.)

---
+++ pathway/CssParser/AntlrWorks.bat
@@ -0,0 +1,3 @@
+rem jdk (java development environment) must be installed and bin folder on path for debugging.
+set path=C:\Program Files (x86)\Java\jdk1.6.0_20\bin;%path%
+java -jar Jars\antlrworks-1.1.7.jar
