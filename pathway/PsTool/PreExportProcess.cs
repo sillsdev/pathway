dce2e5eab07f4acce26230d7c3654085d723af20
Karthik <vkarthim@gmail.com>
2012-05-09 16:36:47 +0530

    Code added for the task
    TD-2959(load the pictures and sound files)

--- pathway/PsTool/PreExportProcess.cs
+++ pathway/PsTool/PreExportProcess.cs
@@ -2577,6 +2577,72 @@ namespace SIL.Tool
 
         }
 
+
+        public List<string> GetImageandAudioFiles(string xhtmlPath)
+        {
+            PublicationInformation projI = new PublicationInformation();
+            projI.DefaultXhtmlFileWithPath = xhtmlPath;
+
+            List<string> imageList = new List<string>();
+
+            if (!File.Exists(projI.DefaultXhtmlFileWithPath)) return imageList;
+
+            var xmldoc = new XmlDocument();
+
+            try
+            {
+                xmldoc = new XmlDocument {XmlResolver = null, PreserveWhitespace = true};
+                xmldoc.Load(projI.DefaultXhtmlFileWithPath);
+                string tag = "img";
+                XmlNodeList nodeList = xmldoc.GetElementsByTagName(tag);
+                if (nodeList.Count > 0)
+                {
+                    foreach (XmlNode item in nodeList)
+                    {
+                        if (item.Attributes != null)
+                        {
+                            var name = item.Attributes.GetNamedItem("src");
+                            if (name != null && name.Value.Length > 0)
+                            {
+                                imageList.Add(name.Value);
+                            }
+
+                        }
+                    }
+                }
+                tag = "span";
+                nodeList = xmldoc.GetElementsByTagName(tag);
+                if (nodeList.Count > 0)
+                {
+                    foreach (XmlNode item in nodeList)
+                    {
+                        if (item.Attributes != null && item.Attributes["lang"] != null)
+                        {
+                            string name = item.Attributes["lang"].Value;
+                            if (name != null && name.IndexOf("-audio") > 0)
+                            {
+                                string audioFile = "AudioVisual/" + item.InnerText;
+                                if (!imageList.Contains(audioFile))
+                                    imageList.Add(audioFile);
+                            }
+
+                        }
+
+                    }
+
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+            }
+            return imageList;
+
+        }
+
+
+
         #endregion
 
         #region XML PreProcessor
