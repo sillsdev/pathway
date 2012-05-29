1b29874ca7671ea84cfb28eacad98c3ffbe68cb8
gtryus <gtryus@gmail.com>
2012-05-29 10:47:42 -0500

    remove dependency on /usr/bin/csharp

--- pathway/ConfigurationTool.sh
+++ pathway/ConfigurationTool.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p ~/.mono/registry/CurrentUser/software/sil/pathway
-cp /etc/mono/registry/LocalMachine/software/sil/pathway/* ~/.mono/registry/CurrentUser/software/sil/pathway
+python -c 'print "<values>\n<value name=\"PathwayDir\"\ntype=\"string\">/usr/lib/pathway/</value>\n<value name=\"WritingSystemStore\"\ntype=\"string\">~/.local/share/SIL/WritingSystemStore/</value>\n</values>"' >~/.mono/registry/CurrentUser/software/sil/pathway/values.xml
+#cp /etc/mono/registry/LocalMachine/software/sil/pathway/* ~/.mono/registry/CurrentUser/software/sil/pathway
 exec /usr/bin/mono /usr/lib/pathway/ConfigurationTool.exe "$@"
 
