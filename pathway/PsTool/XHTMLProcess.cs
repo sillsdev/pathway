a769624b487a5a2663fc7f9c46642d343d27a732
unknown <Sankar@Sankar-Laptop.(none)>
2012-09-03 22:23:59 +0530

    Completed TD-3159 French Regional settings causes large numbers and small pictures

--- pathway/PsTool/XHTMLProcess.cs
+++ pathway/PsTool/XHTMLProcess.cs
@@ -1212,13 +1212,14 @@ namespace SIL.PublishingSolution
                     }
                     if (lineHeight > 0)
                     {
-                        _tempStyle[property.Key] = lineHeight + "pt";
+                        _tempStyle[property.Key] = Common.USANumberFormat(lineHeight.ToString()) + "pt";
                     }
                 }
                 else if (property.Value.IndexOf("%") > 0 && property.Key != "text-position")
                 {
                     float value = float.Parse(property.Value.Replace("%", ""));
-                    _tempStyle[property.Key] = (ancestorFontSize * value / 100).ToString();
+                    float newValue = ancestorFontSize * value / 100;
+                    _tempStyle[property.Key] = Common.USANumberFormat(newValue.ToString()); 
                     const string point = "pt";
                     if (_outputType == Common.OutputType.ODT)
                     {
@@ -1228,7 +1229,8 @@ namespace SIL.PublishingSolution
                             if (_dictColumnGapEm.ContainsKey(secClass) && _dictColumnGapEm[secClass].ContainsKey("columnGap"))
                             {
                                 _dictColumnGapEm[secClass]["columnGap"] = _tempStyle[property.Key] + "pt";
-                                _tempStyle[property.Key] = (-ancestorFontSize * value / 100).ToString();
+                                newValue = -ancestorFontSize * value / 100;
+                                _tempStyle[property.Key] = Common.USANumberFormat(newValue.ToString());
 
                                 if (true)
                                 {
@@ -1246,7 +1248,7 @@ namespace SIL.PublishingSolution
                         else
                         {
                             float size = ancestorFontSize * value / 100;
-                            _tempStyle[property.Key] = size + point;
+                            _tempStyle[property.Key] = Common.USANumberFormat(size.ToString()) + point;
                         }
                     }
                 }
@@ -1256,54 +1258,6 @@ namespace SIL.PublishingSolution
                     _tempStyle[property.Key] = property.Value;
                 }
             }
-            WordCharSpace(ancestorFontSize);
-        }
-
-        private void AssignPropertyOLD(string cssStyleName, float ancestorFontSize)
-        {
-            if (!IdAllClass.ContainsKey(cssStyleName))
-            {
-                return;
-            }
-
-            foreach (KeyValuePair<string, string> property in IdAllClass[cssStyleName])
-            {
-                if (_tempStyle.ContainsKey(property.Key)) continue;
-                if (property.Value.IndexOf("%") > 0)
-                {
-                    float value = float.Parse(property.Value.Replace("%", ""));
-                    _tempStyle[property.Key] = (ancestorFontSize * value / 100).ToString();
-                    const string point = "pt";
-                    if (_outputType != Common.OutputType.IDML)
-                    {
-                        float size = ancestorFontSize * value / 100;
-                        if (property.Key == "line-height")
-                        {
-                            float basepoint = size - ancestorFontSize;
-                            if (basepoint <= 0)
-                                basepoint = ancestorFontSize; // if 1eem or 0em
-                            else if (basepoint < ancestorFontSize)
-                                basepoint = size; // 110%
-                            size = basepoint;
-                        }
-                        _tempStyle[property.Key] = size + point;
-
-                        if (property.Key == "column-gap") // For column-gap: 2em; change the value as (-ve)
-                        {
-                            _dictColumnGapEm["Sect_" + _className.Trim()]["columnGap"] = _tempStyle[property.Key];
-                            _tempStyle[property.Key] = (-ancestorFontSize * value / 100).ToString();
-                        }
-                    }
-                }
-                else
-                {
-                    _tempStyle[property.Key] = property.Value;
-                }
-            }
-            //if (IdAllClass[cssStyleName].ContainsKey("TextColumnGutter"))
-            //{
-            //    IdAllClass[cssStyleName]["TextColumnGutter"] = _tempStyle["TextColumnGutter"];
-            //}
             WordCharSpace(ancestorFontSize);
         }
 
@@ -1544,7 +1498,7 @@ namespace SIL.PublishingSolution
                 {
                     float value = float.Parse(currentFontSize.Replace("%", ""));
                     fontSize = fontSize * value / 100;
-                    IdAllClass[_classNameWithLang][fontPointSize] = fontSize.ToString();
+                    IdAllClass[_classNameWithLang][fontPointSize] = Common.USANumberFormat(fontSize.ToString());
                 }
                 else if (currentFontSize == "larger" || currentFontSize == "smaller")
                 {
