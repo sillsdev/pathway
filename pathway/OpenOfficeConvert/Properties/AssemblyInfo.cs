2cc21c830b2e40cf7449d5c97cffe99f05694f23
Greg Trihus <gtryus@gmail.com>
2012-10-24 20:45:46 -0500

    Pathway 1.3.2 sprint release changes

--- pathway/OpenOfficeConvert/Properties/AssemblyInfo.cs
+++ pathway/OpenOfficeConvert/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.1.2686*")]
-[assembly: AssemblyVersion("1.3.1.2686")]
-[assembly: AssemblyFileVersion("1.3.1.2686")]
+// [assembly: AssemblyVersion("1.3.2.2752*")]
+[assembly: AssemblyVersion("1.3.2.2752")]
+[assembly: AssemblyFileVersion("1.3.2.2752")]
