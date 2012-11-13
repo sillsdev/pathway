ee050ceff5d59596c4722cf80dbf47bbea2dfc6a
Greg Trihus <gtryus@gmail.com>
2012-11-13 11:20:54 -0600

    Updates for 1.3.3 Sprint release

--- pathway/PublishingSolutionExe/Properties/AssemblyInfo.cs
+++ pathway/PublishingSolutionExe/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.2.2752")]
-[assembly: AssemblyFileVersion("1.3.2.2752")]
+[assembly: AssemblyVersion("1.3.3.2794")]
+[assembly: AssemblyFileVersion("1.3.3.2794")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
