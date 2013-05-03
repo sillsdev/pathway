13b5c340d8a75811298f46dd66be22f7d69acc76
jamesprabud <jamesprabud@gmail.com>
2013-05-03 12:53:27 +0530

    Color Logo added for TD-3488

--- pathway/CssDialog/Properties/Resources2.Designer.cs
+++ pathway/CssDialog/Properties/Resources2.Designer.cs
@@ -39,7 +39,7 @@ namespace SIL.PublishingSolution.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SIL.PublishingSolution.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SIL.PublishingSolution.Properties.Resources", typeof(Resources1).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
