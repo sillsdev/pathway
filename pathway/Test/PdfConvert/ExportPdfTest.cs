352f07a430822744c69f8534e0552aebb0989e3e
Samdoss <sam147doss@gmail.com>
2013-10-22 20:37:14 +0530

    TD-3666 Book with Headerbased on Reference format

---
+++ pathway/Test/PdfConvert/ExportPdfTest.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using SIL.PublishingSolution;
+using NUnit.Framework;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using SIL.Tool;
+
+namespace Test.PdfConvert
+{
+    /// <summary>
+    ///This is a test class for MergeCssTest and is intended
+    ///to contain all MergeCssTest Unit Tests
+    ///</summary>
+    [TestFixture]
+    public class ExportPdfTest
+    {
+        private string _inputPath;
+        private string _outputPath;
+        private string _expectedPath;
+        private string _testFolderPath = string.Empty;
+        private PublicationInformation _projInfo;
+
+
+        #region Setup
+        [TestFixtureSetUp]
+        protected void SetUpAll()
+        {
+            Common.Testing = true;
+            _projInfo = new PublicationInformation();
+            _testFolderPath = PathPart.Bin(Environment.CurrentDirectory, "/PdfConvert/TestFiles");
+            _inputPath = Common.PathCombine(_testFolderPath, "input");
+            _outputPath = Common.PathCombine(_testFolderPath, "output");
+            _expectedPath = Common.PathCombine(_testFolderPath, "Expected");
+            const bool recursive = true;
+            if (Directory.Exists(_outputPath))
+                Directory.Delete(_outputPath, recursive);
+            Directory.CreateDirectory(_outputPath);
+            _projInfo.ProjectPath = _testFolderPath;
+            Common.SupportFolder = "";
+        }
+
+        #endregion
+
+        #region Public Functions
+
+        #region Nunits
+
+        [Test]
+        [Category("SkipOnTeamCity")]
+        public void ScriptureReplaceBookNametoBookCodeTest()
+        {
+            _projInfo.ProjectInputType = "Scripture";
+            const string file = "ScripturePreview";
+            ExportProcess(file);
+
+            string outputXhtmlFile = Path.Combine(_outputPath, file + ".xhtml");
+            File.Copy(_projInfo.DefaultXhtmlFileWithPath, outputXhtmlFile, true);
+
+            ExportPdf pdfObj = new ExportPdf();
+            pdfObj.ReplaceBookNametoBookCode(outputXhtmlFile);
+
+            FileCompare(file);
+        }
+
+        #endregion
+
+        #region Private Functions
+
+        private void FileCompare(string file)
+        {
+            string texOutput = FileOutput(file + ".xhtml");
+            string texExpected = FileExpected(file + ".xhtml");
+            TextFileAssert.AreEqual(texOutput, texExpected, file + " in xhtml ");
+        }
+
+        private void ExportProcess(string file)
+        {
+            string input = FileInput(file + ".xhtml");
+            _projInfo.DefaultXhtmlFileWithPath = input;
+            input = FileInput(file + ".css");
+            _projInfo.DefaultCssFileWithPath = input;
+            _projInfo.TempOutputFolder = _outputPath;
+            _projInfo.OutputExtension = ".xhtml";
+        }
+
+        private string FileInput(string fileName)
+        {
+            return Common.PathCombine(_inputPath, fileName);
+        }
+
+        private string FileOutput(string fileName)
+        {
+            return Common.PathCombine(_outputPath, fileName);
+        }
+
+        private string FileExpected(string fileName)
+        {
+            return Common.PathCombine(_expectedPath, fileName);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
