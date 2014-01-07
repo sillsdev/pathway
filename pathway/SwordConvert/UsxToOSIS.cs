a621d63d0bd3bbbe1b97039780d00cb98de50bab
Sankar Venkat <mcasankar@gmail.com>
2014-01-07 16:01:13 +0530

    TD-1323c (Sword output) Project Included

--- pathway/SwordConvert/UsxToOSIS.cs
+++ pathway/SwordConvert/UsxToOSIS.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
-using System.Windows.Forms;
 using System.Xml;
 using Microsoft.Win32;
 using SIL.Tool;
