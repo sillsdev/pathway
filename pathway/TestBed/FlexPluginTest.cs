4ce178d5f258db2a64c1a2b10870e38e78c036ed
Samdoss <sam147doss@gmail.com>
2014-09-26 19:17:38 +0530

    TD-4194 Add Nunit testcase based on dotcover

--- pathway/TestBed/FlexPluginTest.cs
+++ pathway/TestBed/FlexPluginTest.cs
@@ -429,11 +429,11 @@ namespace TestBed
         private void StyConvert_Click(object sender, EventArgs e)
         {
 #if !Not7
-            StyToCss styToCss = new StyToCss();
-            styToCss.StyFullPath = txtCSSInput.Text;
-            string outputCSS = txtCSSInput.Text.Replace(".sty", ".css");
-            styToCss.ConvertStyToCss("TestBed", outputCSS);
-            MessageBox.Show("Exported in " + outputCSS);
+            //StyToCss styToCss = new StyToCss();
+            //styToCss.StyFullPath = txtCSSInput.Text;
+            //string outputCSS = txtCSSInput.Text.Replace(".sty", ".css");
+            //styToCss.ConvertStyToCss("TestBed", outputCSS);
+            //MessageBox.Show("Exported in " + outputCSS);
 #endif
         }
 
