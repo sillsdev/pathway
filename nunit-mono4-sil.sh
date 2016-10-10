1d6fd6aee17fe77e3c4658192d51084cfc4c3063
gtryus <gtryus@gmail.com>
2016-10-10 15:52:26 -0500

    Script to run nunit with .net 4.6.1

---
+++ nunit-mono4-sil.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export MONO_PREFIX=/opt/mono4-sil
+export GDK_SHARP=${MONO_PREFIX}/lib/mono/gtk-sharp-3.0
+export LD_LIBRARY_PATH=${MONO_PREFIX}/lib:${LD_LIBRARY_PATH}
+export PKG_CONFIG_PATH=${MONO_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+export MONO_GAC_PREFIX=${MONO_PREFIX}:${MONO_GAC_PREFIX}
+export MONO_MWF_SCALING=disable
+export PATH=${MONO_PREFIX}/bin:$PATH
+nunit-console output/Debug/Test.dll -xml=Test-Result.xml -labels -out=Test-Output.txt -exclude=SkipOnTeamCity\;SkipOnLinux
