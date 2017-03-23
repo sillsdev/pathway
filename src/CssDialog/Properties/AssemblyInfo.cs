7c10d3fa8c25d32b71b32afa10182906f7b2136e
TrihusG <Trihus@dallas.sil.org>
2017-03-23 11:04:12 -0500

    Update version numbers

--- src/CssDialog/Properties/AssemblyInfo.cs
+++ src/CssDialog/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.13.4.4657")]
-[assembly: AssemblyFileVersion("1.13.4.4657")]
+[assembly: AssemblyVersion("1.15.7.5693")]
+[assembly: AssemblyFileVersion("1.15.7.5693")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
--- src/CssParser/Properties/AssemblyInfo.cs
+++ src/CssParser/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.13.4.4657")]
-[assembly: AssemblyFileVersion("1.13.4.4657")]
+[assembly: AssemblyVersion("1.15.7.5693")]
+[assembly: AssemblyFileVersion("1.15.7.5693")]
