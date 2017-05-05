4b5a85a2ff5135c5fac9e6a43f2bcccf38db0b9e
gtryus <gtryus@gmail.com>
2017-05-05 11:50:14 -0500

    Update Linux test runner scripts

--- Build/nunit-gui-mono4-sil.sh
+++ Build/nunit-gui-mono4-sil.sh
@@ -6,4 +6,4 @@ export PKG_CONFIG_PATH=${MONO_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
 export MONO_GAC_PREFIX=${MONO_PREFIX}:${MONO_GAC_PREFIX}
 export MONO_MWF_SCALING=disable
 export PATH=${MONO_PREFIX}/bin:$PATH
-nunit-console output/Debug/Export/Test.dll -xml=Test-Result.xml -labels -out=Test-Output.txt -exclude=SkipOnTeamCity\;SkipOnLinux
+mono ../packages/NUnit.Runners.Net4.2.6.4/tools/nunit-x86.exe Pathway.nunit
