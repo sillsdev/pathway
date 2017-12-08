b823e25f767816488db2613320740ac93523b75a
gtryus <gtrihus59@yahoo.com>
2017-12-08 07:22:05 -0600

    TD-4887 Running Pathway from Flex 64-bit (#11)
    Create a FieldWorks 9 caller type
    Change projects Fieldworks calls to Any CPU
    Change dependencies of Fieldworks calls to Any CPU\
    Fix access error that tries to update file in program folder
    Fall back for namespaces to localize to handle load error
    Adjust backend loading to get names using subprocess

--- src/Test/epubConvert/EpubDictionaryExportTest.cs
+++ src/Test/epubConvert/EpubDictionaryExportTest.cs
@@ -65,7 +65,7 @@ namespace Test.epubConvert
 			projInfo.DefaultRevCssFileWithPath = Common.PathCombine(_inputPath, "FlexRev.css");
 			projInfo.ProjectName = "EBook (epub)_" + DateTime.Now.Date.ToShortDateString() + "_" +
 								   DateTime.Now.Date.ToShortTimeString();
-			DataCreator.Creator = DataCreator.CreatorProgram.FieldWorks;
+			DataCreator.Creator = DataCreator.CreatorProgram.FieldWorks8;
 			using (Common.CallerSetting = new CallerSetting {SettingsFullPath = projInfo.DefaultXhtmlFileWithPath})
 			{
 				var target = new Exportepub();
