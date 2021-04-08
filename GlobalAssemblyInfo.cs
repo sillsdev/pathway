eca7eb42dbd9f80cc32640b5143f8589c46aa7b5
Greg Trihus <greg_trihus@sil.org>
2021-04-08 15:33:34 -0500

    Updates for latest version

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,14 +1,14 @@
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyCompany("SIL")]
 [assembly: AssemblyProduct("Pathway")]
-[assembly: AssemblyCopyright("Copyright © 2008-2016 SIL International")]
+[assembly: AssemblyCopyright("Copyright © 2008-2020 SIL International")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyFileVersion("1.18.0.5979")]
-[assembly: AssemblyVersion("1.18.0.5979")]
+[assembly: AssemblyFileVersion("1.18.0.5984")]
+[assembly: AssemblyVersion("1.18.0.5984")]
 
